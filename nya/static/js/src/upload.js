9467d6a89d18df6732e57572f693be2b65ee21c6
boreq <boreq@sourcedrops.com>
2018-01-09 03:02:46 +0100

    Drag and drop, the website is wider

--- nya/static/js/src/upload.js
+++ nya/static/js/src/upload.js
@@ -1,31 +1,79 @@
-var errorDiv, uploadList, progressBar, spinner;
+var errorDiv, uploadList, progressBar, spinner, uploadInput, uploadButton, dropZone;
+
+
+/*
+    A separate div called "#drop-zone" is needed because of the problems related
+    to dropping the files on the text label instead of the parent button.
+
+    While the click event is not stopped by the fact that the child element
+    covers the parent element, the same situation is a problem when the drop
+    event occurs.
+*/
 
 
 $(function() {
-    var uploadButton = $('#upload-button'),
-        uploadInput = $('#upload-input');
+    uploadButton = $('#upload-button');
+    uploadInput = $('#upload-input');
     errorDiv = $('#upload-error');
     uploadList = $('#upload-list');
     progressBar = $('progress');
     spinner = $('#spinner');
+    dropZone = $('#drop-zone');
 
+    // Clicking on the button.
     uploadButton.on('click', function(e) {
         e.preventDefault();
         uploadInput.trigger('click');
     });
 
     uploadInput.on('change', fileSelectHandler);
+
+    // Dropping the files
+    dropZone.on('dragover dragenter', function() {
+        uploadButton.addClass('is-dragover');
+    })
+    .on('dragleave dragend drop', function() {
+        uploadButton.removeClass('is-dragover');
+    })
+    .on('dragover', function(e) {
+        e.preventDefault();
+    })
+    .on('dragend', function(e) {
+        // Remove all of the drag data
+        var dt = e.dataTransfer;
+        if (dt.items) {
+            // Use DataTransferItemList interface to remove the drag data
+            for (var i = 0; i < dt.items.length; i++) {
+                dt.items.remove(i);
+            }
+        } else {
+            // Use DataTransfer interface to remove the drag data
+            e.dataTransfer.clearData();
+        }
+    });
+
+    dropZone.on('drop', fileDropHandler);
 });
 
 
 function fileSelectHandler(e) {
     var formData = new FormData();
-
     for (var i = 0; f = this.files[i]; i++) {
-        formData.append(this.getAttribute('name'), f);
+        formData.append(uploadInput.attr('name'), f);
     }
     formData.append('expires', 0);
+    upload(formData);
+}
 
+
+function fileDropHandler(e) {
+    e.preventDefault();
+
+    var formData = new FormData();
+    $.each(e.originalEvent.dataTransfer.files, function(i, f) {
+        formData.append(uploadInput.attr('name'), f);
+    });
+    formData.append('expires', 0);
     upload(formData);
 }
 
